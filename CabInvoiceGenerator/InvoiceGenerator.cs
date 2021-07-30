5c927e0a1cb97fe4ce9401b6d81f7c4547a07840
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-07-30 17:31:39 +0530

    [SaiVijayaKumar] Calculateing Fare For Normal Ride

---
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceGenerator
+    {
+        RideType rideType;
+        //creating values for readonly mode
+        private readonly double MINIMUM_COST_PER_KM;
+        private readonly int COST_PER_TIME;
+        private readonly double MINIMUM_FARE;
+
+        //parameter constructure
+        public InvoiceGenerator(RideType rideType)
+        {
+            this.rideType = rideType;
+            try
+            {
+                this.MINIMUM_COST_PER_KM = 10;
+                this.COST_PER_TIME = 1;
+                this.MINIMUM_FARE = 5;
+            }
+            catch(CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_RIDETYPE, "Invalid Raid Type");
+            }
+        }
+
+        //Calculating Fare For Single Rider by using distance and time.
+        public double CalculateFare(double distance,int time)
+        {
+            double totalFare = 0;
+            try
+            {
+                totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+            }
+            catch(CabInvoiceException)
+            {
+                if(distance <= 0)
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_DISTANCE, "Invalid Distance");
+                }
+                if(time <= 0)
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_TIME, "Invalid Time");
+                }
+            }
+            return Math.Max(totalFare, MINIMUM_FARE);
+        }
+    }
+}
---
+++ UnitTestForCabInvoiceGenerator/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using CabInvoiceGenerator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestForCabInvoiceGenerator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("UC1")]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORAML);
+            double distance = 3.9;
+            int time = 4;
+            //calculating fare
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double excepted = 43;
+            Assert.AreEqual(excepted, fare);
+        }
+    }
+}
